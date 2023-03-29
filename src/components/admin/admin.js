9570a1288b0829c2f3e08d44a9aab5a45a114a21
mu-irshad011 <smoirshad11@gmail.com>
2023-03-29 15:37:43 +0530

    form validarion added, data add to database also working now

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -1,44 +1,24 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './admin.sass'
 import supabase from '../../supabase/supabaseClient.js'
 import * as Yup from 'yup';
 import { useFormik } from 'formik';
 
 const Admin = () => {
-    const [color, setColor] = useState('');
-    const [rate, setRate] = useState('');
-    const [parcel, setParcel] = useState({
-        receipt_no: '',
-        sender_name: '',
-        receiver_name: '',
-        sender_number: '',
-        receiver_number: '',
-        item_detail: '',
-        quantity: '',
-        total_amount: '',
-        payment_type: '',
-        place_to_send: '',
-        remarks: '',
-        driver: ''
-    })
-
-    const formSchema = Yup.object({
-        sender_name: Yup.string().required("Enter Sender Name")
-    })
-
-    const onPrintAndSave = async () => {
-        const { data, error } = await supabase.from("parcels").insert({ ...parcel })
-        if (!error) {
-            console.log("data: ", data);
-            window.location.reload(false);
-        } else {
-            console.log("error: ", error);
-            throw new Error(error);
-        }
-    }
-
     const validate = Yup.object().shape({
-        sender_name: Yup.string().required("Sender name is required")
+        sender_name: Yup.string().required("Please enter Sender name"),
+        sender_number: Yup.string().required("Please enter Sender number"),
+        receiver_name: Yup.string().required("Please enter Receiver name"),
+        receiver_number: Yup.string().required("Please enter Receiver number"),
+        item_detail: Yup.string().required("Please select item detail"),
+        color: Yup.string().required("Please select item color"),
+        quantity: Yup.string().required("Please enter qunatity"),
+        rate: Yup.string().required("Please enter rate"),
+        total_amount: Yup.string().required("Please enter total amount"),
+        payment_type: Yup.string().required("Please select Payment type"),
+        place_to_send: Yup.string().required("Please select Place to Send"),
+        remarks: Yup.string().required("Please enter remarks"),
+        driver: Yup.string().required("Please enter driver number")
     })
 
     const formik = useFormik({
@@ -48,17 +28,26 @@ const Admin = () => {
             receiver_name: '',
             receiver_number: '',
             item_detail: '',
+            color: '',
             quantity: '',
             rate: '',
             payment_type: '',
             total_amount: '',
             place_to_send: '',
             remarks: '',
-            drive: ''
+            driver: ''
         },
         validationSchema: validate,
         onSubmit: async (values) => {
             console.log("values: ", values)
+            const { data, error } = await supabase.from("parcels").insert({ ...values })
+            if (!error) {
+                console.log("data: ", data);
+                window.location.reload(false);
+            } else {
+                console.log("error: ", error);
+                throw new Error(error);
+            }
         }
     })
 
@@ -73,7 +62,7 @@ const Admin = () => {
                                 <input
                                     name='sender_name'
                                     value={formik.values.sender_name}
-                                    onChange={(e) => { setParcel(prev => ({ ...prev, sender_name: e.target.value })); formik.handleChange(e) }}
+                                    onChange={formik.handleChange}
                                 />
                                 {
                                     formik.errors.sender_name && <div className='text-danger'>{formik.errors.sender_name}</div>
@@ -84,27 +73,39 @@ const Admin = () => {
                             <div className='form_control_wrapper'>
                                 <label>Sender Number</label>
                                 <input
-                                    value={parcel.sender_number}
-                                    onChange={(e) => setParcel(prev => ({ ...prev, sender_number: e.target.value }))}
+                                    name='sender_number'
+                                    value={formik.values.sender_number}
+                                    onChange={formik.handleChange}
                                 />
+                                {
+                                    formik.errors.sender_number && <div className='text-danger'>{formik.errors.sender_number}</div>
+                                }
                             </div>
                         </div>
                         <div className='col-30'>
                             <div className='form_control_wrapper'>
                                 <label>Receiver Name</label>
                                 <input
-                                    value={parcel.receiver_name}
-                                    onChange={(e) => setParcel(prev => ({ ...prev, receiver_name: e.target.value }))}
+                                    name='receiver_name'
+                                    value={formik.values.receiver_name}
+                                    onChange={formik.handleChange}
                                 />
+                                {
+                                    formik.errors.receiver_name && <div className='text-danger'>{formik.errors.receiver_name}</div>
+                                }
                             </div>
                         </div>
                         <div className='col-2'>
                             <div className='form_control_wrapper'>
                                 <label>Receiver Number</label>
                                 <input
-                                    value={parcel.receiver_number}
-                                    onChange={(e) => setParcel(prev => ({ ...prev, receiver_number: e.target.value }))}
+                                    name='receiver_number'
+                                    value={formik.values.receiver_number}
+                                    onChange={formik.handleChange}
                                 />
+                                {
+                                    formik.errors.receiver_number && <div className='text-danger'>{formik.errors.receiver_number}</div>
+                                }
                             </div>
                         </div>
                     </div>
@@ -112,23 +113,31 @@ const Admin = () => {
                         <div className='col-30'>
                             <div className='form_control_wrapper'>
                                 <label>Item Details</label>
-                                <select name="cars" id="cars" value={parcel.item_detail} onChange={(e) => { setParcel(prev => ({ ...prev, item_detail: color + " " + e.target.value })) }}>
+                                <select name="item_detail" id="cars" value={formik.values.item_detail} onChange={formik.handleChange}>
+                                    <option value="">Select Item detail...</option>
                                     <option value="Volvo">Volvo</option>
                                     <option value="Saab">Saab</option>
                                     <option value="Mercedes">Mercedes</option>
                                     <option value="Audi">Audi</option>
                                 </select>
+                                {
+                                    formik.errors.item_detail && <div className='text-danger'>{formik.errors.item_detail}</div>
+                                }
                             </div>
                         </div>
                         <div className='col-30'>
                             <div className='form_control_wrapper'>
                                 <label>Colors</label>
-                                <select name="colors" id="colors" onChange={(e) => { setParcel(prev => ({ ...prev, item_detail: e.target.value + " " + parcel.item_detail })); setColor(e.target.value) }}>
+                                <select name="color" id="color" value={formik.values.color} onChange={formik.handleChange}>
+                                    <option value="">Select Color...</option>
                                     <option value="Red">Red</option>
                                     <option value="Green">Green</option>
                                     <option value="Blue">Blue</option>
                                     <option value="Cyan">Cyan</option>
                                 </select>
+                                {
+                                    formik.errors.color && <div className='text-danger'>{formik.errors.color}</div>
+                                }
                             </div>
                         </div>
                     </div>
@@ -137,49 +146,69 @@ const Admin = () => {
                             <div className='form_control_wrapper'>
                                 <label>Quantity Number</label>
                                 <input
-                                    value={parcel.quantity}
-                                    onChange={(e) => setParcel(prev => ({ ...prev, quantity: e.target.value }))}
+                                    name='quantity'
+                                    value={formik.values.quantity}
+                                    onChange={formik.handleChange}
                                 />
+                                {
+                                    formik.errors.quantity && <div className='text-danger'>{formik.errors.quantity}</div>
+                                }
                             </div>
                         </div>
                         <div className='col-2'>
                             <div className='form_control_wrapper'>
                                 <label>Rate</label>
                                 <input
-                                    value={rate}
-                                    onChange={(e) => setRate(e.target.value)}
+                                    name='rate'
+                                    value={formik.values.rate}
+                                    onChange={formik.handleChange}
                                 />
+                                {
+                                    formik.errors.rate && <div className='text-danger'>{formik.errors.rate}</div>
+                                }
                             </div>
                         </div>
                         <div className='col-2'>
                             <div className='form_control_wrapper'>
                                 <label>Total Amount</label>
                                 <input
-                                    value={parcel.total_amount}
-                                    onChange={(e) => setParcel(prev => ({ ...prev, total_amount: e.target.value }))}
+                                    name='total_amount'
+                                    value={formik.values.total_amount}
+                                    onChange={formik.handleChange}
                                 />
+                                {
+                                    formik.errors.total_amount && <div className='text-danger'>{formik.errors.total_amount}</div>
+                                }
                             </div>
                         </div>
                         <div className='col-2'>
                             <div className='form_control_wrapper'>
                                 <label>Payment Type</label>
-                                <select name="Payment Type" id="payment_type" onChange={(e) => setParcel(prev => ({ ...prev, payment_type: e.target.value }))}>
+                                <select name="payment_type" id="payment_type" value={formik.values.payment_type} onChange={formik.handleChange}>
+                                    <option value="">Select Payment Type...</option>
                                     <option value="Baki">Baki</option>
                                     <option value="Jama">Jama</option>
                                     <option value="Account">Account</option>
                                     <option value="Free of Charge">Free of Charge</option>
                                 </select>
+                                {
+                                    formik.errors.payment_type && <div className='text-danger'>{formik.errors.payment_type}</div>
+                                }
                             </div>
                         </div>
                         <div className='col-2'>
                             <div className='form_control_wrapper'>
                                 <label>Place to send</label>
-                                <select name="cars" id="cars" onChange={(e) => setParcel(prev => ({ ...prev, place_to_send: e.target.value }))}>
+                                <select name="place_to_send" id="cars" value={formik.values.place_to_send} onChange={formik.handleChange}>
+                                    <option value="">Select Place to Send...</option>
                                     <option value="volvo">Volvo</option>
                                     <option value="saab">Saab</option>
                                     <option value="mercedes">Mercedes</option>
                                     <option value="audi">Audi</option>
                                 </select>
+                                {
+                                    formik.errors.place_to_send && <div className='text-danger'>{formik.errors.place_to_send}</div>
+                                }
                             </div>
                         </div>
                     </div>
@@ -189,24 +218,31 @@ const Admin = () => {
                             <div className='form_control_wrapper'>
                                 <label>Remarks</label>
                                 <input
-                                    value={parcel.remarks}
-                                    onChange={(e) => setParcel(prev => ({ ...prev, remarks: e.target.value }))}
+                                    name='remarks'
+                                    value={formik.values.remarks}
+                                    onChange={formik.handleChange}
                                 />
+                                {
+                                    formik.errors.remarks && <div className='text-danger'>{formik.errors.remarks}</div>
+                                }
                             </div>
                         </div>
                         <div className='col-4'>
                             <div className='form_control_wrapper'>
-                                <label>Drive</label>
+                                <label>Driver</label>
                                 <input
-                                    value={parcel.driver}
-                                    onChange={(e) => setParcel(prev => ({ ...prev, driver: e.target.value }))}
+                                    name='driver'
+                                    value={formik.values.driver}
+                                    onChange={formik.handleChange}
                                 />
+                                {
+                                    formik.errors.driver && <div className='text-danger'>{formik.errors.driver}</div>
+                                }
                             </div>
                         </div>
                     </div>
                     <div className="row justify-between mt-30">
                         <div className="col-12">
-
                             <button type='submit' className='pt__lr_num time_btn btn btn-submit'>Print and Save</button>
                         </div>
                     </div>
