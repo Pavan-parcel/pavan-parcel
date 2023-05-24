62509e092c97fbba3297221398ac2e43b6d285dd
Rajgaudani <100108395+VrajGaudani@users.noreply.github.com>
2023-05-24 21:57:43 +0530

    loader

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,13 +2,16 @@ import React from 'react'
 import Time from '../time/time'
 import Admin from '../admin/admin'
 import Table from '../table/table'
+import Loader from '../loader/loader'
 
 const Home = () => {
   return (
     <>
+    <Loader />
     <Time />
     <Admin />
     <Table />
+
     </>
   )
 }
